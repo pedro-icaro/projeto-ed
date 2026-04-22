60838f952f689958b0f5d2feff1336e930785d62
pedro-icaro <icaropedro751@gmail.com>
2026-04-22 20:00:34 -0300

    Adicionando titulo da pagina de forma dinamica (incompleto)

--- app/components/home/[id]/page.tsx
+++ app/components/home/[id]/page.tsx
@@ -1,7 +1,18 @@
-export default function Detalhescursos(){
+import { Metadata } from "next"
+
+interface Props {
+    params: Promise<{id:string}>
+}
+
+export const  metadata: Metadata = {
+    title:"Html 5",
+}
+
+export default async function Detalhescursos({params}: Props){
+    const {id} = await params;
     return(
         <>
-        <h1>Detalhe do curso</h1>
+        <h1>Detalhe do curso {id} </h1>
         </>
     )
 }
